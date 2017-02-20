2b3467f43b347800045d65e9221c8e8ed17a19ce
RinsibleElk <oliver.samson@gmail.com>
2017-02-20 01:16:35 +0000

    MahApps but lost the scrolling

--- Nemo.Wpf/App.fs
+++ Nemo.Wpf/App.fs
@@ -5,6 +5,7 @@ open System.Windows
 open System.Windows.Controls
 open System.Windows.Markup
 open FsXaml
+open MahApps.Metro
 
 type App = XAML<"App.xaml">
 
@@ -14,4 +15,12 @@ type App = XAML<"App.xaml">
 let main(_) =
     let mainViewModel = MainViewModel()
     let app = App()
+    app.Activated
+    |> Event.add
+        (fun _ ->
+            let appStyle = ThemeManager.DetectAppStyle(Application.Current)
+            ThemeManager.ChangeAppStyle(Application.Current,
+                                        ThemeManager.GetAccent("Blue"),
+                                        ThemeManager.GetAppTheme("BaseLight"));
+            ())
     app.Run(mainViewModel.View)
